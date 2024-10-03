843c03056c33f5ea86a4b2f3aa6caff6c3df2060
Edward Leon <edleon761@gmail.com>
2024-10-03 00:16:53 -0700

    Stations now implemented
    Set up a timer logic within the placing area script to handle timers and whatnot
    - Currently sets objects to a new material, but that'll be changed eventually to a more general function

--- Assets/Scripts/Other Scripts/Placing Area.cs
+++ Assets/Scripts/Other Scripts/Placing Area.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlacingArea : MonoBehaviour
 {
@@ -9,6 +10,13 @@ public class PlacingArea : MonoBehaviour
     [Header("Current Item Variables")]
     public GameObject currentPick;
     public bool hasItem = false;
+
+    [Header("Station Specific Timers")]
+    public bool inProgress = false;
+    public float timeRunning = 0.0f;
+    public float targetTime = 3f;
+
+    public Material tempMaterial;
 
     public string debugType()
     {
@@ -25,16 +33,58 @@ public class PlacingArea : MonoBehaviour
 
     }
 
+    public void Update()
+    {
+        // If there is a job to be done
+        if (inProgress)
+        {
+            // The update function will increment the run time of a project
+            timeRunning += Time.deltaTime;
+
+            // And once the times up, the code will do something!!!
+            if (timeRunning >= targetTime)
+            {
+
+                // temp line, make this a general do something function later
+                currentPick.gameObject.GetComponent<MeshRenderer>().material = tempMaterial;
+                ResetTimer();
+
+            }
+
+        }
+    }
+
     public void itemGiven(GameObject item)
     {
         currentPick = item;
         hasItem = true;
+        if (type == 2)
+        {
+            StartTimer();
+        }
     }
 
     public void itemTaken()
     {
         currentPick = null;
         hasItem = false;
+
+        if (type == 2)
+        {
+            ResetTimer();
+        }
+
+    }
+
+    public void StartTimer()
+    {
+        inProgress = true;
+        timeRunning = 0.0f;
+    }
+
+    public void ResetTimer()
+    {
+        inProgress = false;
     }
 
 }
