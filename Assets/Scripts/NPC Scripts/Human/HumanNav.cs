727f7e15858357885d48d80108ab3d58fbad8a82
Edward Leon <edleon761@gmail.com>
2024-10-10 01:04:55 -0700

    Humans now contribute to level completion

--- Assets/Scripts/NPC Scripts/Human/HumanNav.cs
+++ Assets/Scripts/NPC Scripts/Human/HumanNav.cs
@@ -18,6 +18,12 @@ public class HumanNav : MonoBehaviour
     private Job job;
     private NavMeshAgent agent;
 
+    // Checks for elements in a certain radius listed here
+    public float radiusCheck = .5f;
+
+    // Checks for elements in a certain layer listed here
+    public LayerMask layerToLookFor;
+
     public bool debug = true;
 
     private void Awake()
@@ -35,6 +41,48 @@ public class HumanNav : MonoBehaviour
     public void SetJobTarget(Transform target)
     {
         job.SetJobStation(target);
-        agent.SetDestination(target.position);
+
+        // if there is a target, then the code will make sure the navmesh gets a human there
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            agent.isStopped = false;
+        }
+        else
+        {
+            // If null was passed in, the nav mesh will stop
+            agent.isStopped = true;
+        }
+        
     }
+
+    public void Update()
+    {
+        // will only check if the human is not currently working
+        if (!job.IsWorking())
+        {
+
+            // Gets an array of colliders that overlap a new sphere in a specific layer
+            Collider[] hitColliders = Physics.OverlapCapsule(new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z),
+                                                             new Vector3(transform.position.x, transform.position.y - 0.5f, transform.position.z),
+                                                                radiusCheck, layerToLookFor);
+            
+            // loops through all the colliders found on a certain layer in an effort to find a work station
+            foreach (var hitCollider in hitColliders)
+            {
+                // Once a station is found, then it's set as the next target
+                if (hitCollider.GetComponent<WorkStation>() != null)
+                {
+                    // If the job hasn't been completed, then and only then will it be chosen as a target
+                    if (!hitCollider.GetComponent<WorkStation>().getStatus())
+                    {
+                        SetJobTarget(hitCollider.transform);
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
+
 }
