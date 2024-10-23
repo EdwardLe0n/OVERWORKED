148d3c3afe7d0c93b564d87092e640fb529a2fec
mbelles21 <mbelles@csumb.edu>
2024-10-23 13:05:19 -0700

    resolved conflicts

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -6,17 +6,6 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     public Slider slider;
-<<<<<<< Updated upstream
-    public Image fill;
-    
-    public void SetMaxValue(float n)
-    {
-        slider.maxValue = n;
-        slider.value = 0; // bc max value is set at beginning of level when no tasks are completed
-    }
-
-    // call when completing tasks
-=======
 
     public void SetMaxValue(float n)
     {
@@ -24,7 +13,6 @@ public class ProgressBar : MonoBehaviour
         slider.value = 0; // default to 0 bc no tasks will be done when level starts
     }
 
->>>>>>> Stashed changes
     public void IncrementValue()
     {
         slider.value += 1;
