1bcafccb5b20e888d82248cbedcc797994dbf66e
AndAgGit <afon.cyffrous@gmail.com>
2024-10-24 10:54:15 -0700

    Fixed PR button spawning and started work on robots stressing humans

---
+++ Assets/Scripts/Player Scripts/RobotsStressHumans.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotsStressHumans : MonoBehaviour
+{
+    [Tooltip("The collider to check")]
+    public SphereCollider stressCollider;
+
+    void Update(){
+        
+    }
+}
